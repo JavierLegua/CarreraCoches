00ecb550c093a17454f578688136f1b92c2d3512
JAVIER LEGUA <javierlegua14@gmail.com>
2021-01-28 10:36:37 +0100

    Constructor Coche Creado

--- src/Carrera/Coche.java
+++ src/Carrera/Coche.java
@@ -9,9 +9,20 @@ public class Coche {
 	private String nomPiloto;
 	private int dorsal;
 	private int distanciaCarrera;
-	//private String estado;
-	private int potencia;
+	private String estado;
+	private final int POTENCIA = 50;
 	private int velocidad;
 	private int distanciaRecorrida;
+	
+	public Coche(String nomPiloto, int dorsal) {
+		this.nomPiloto = nomPiloto = nomPiloto;
+		this.dorsal = dorsal = dorsal;
+		this.distanciaCarrera = 4568;
+		this.estado = "Parado";
+		this.velocidad = 0;
+		this.distanciaRecorrida = 0;
+	}
+	
+	
 	
 }
